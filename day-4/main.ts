dc1b91559b13b96771e543ca02d3deb7d1e22062
Niku Grönberg <gronbergniku@gmail.com>
2024-12-04 21:53:41 +0200

    day 4 second solution

--- day-4/main.ts
+++ day-4/main.ts
@@ -68,30 +68,89 @@ const stepper = (input: string[], target: string) => {
   return checker.matches + reverseChecker.matches;
 };
 
+const xmasFinder = (input: string[], target: string) => {
+  const checker = new Checker(target);
+  const reverseChecker = new Checker(target.split("").reverse().join(""));
+
+  const height = input.length;
+  const width = input[0].length;
+  const allStarts = width + height - 1;
+
+  // Diagonal - Down right
+  for (let start = 0; start < allStarts; ++start) {
+    let x = Math.max(0, start - height + 1);
+    let y = Math.max(0, height - start - 1);
+    while (x < width && y < height) {
+      checker.read(input[y][x], x, y);
+      reverseChecker.read(input[y][x], x, y);
+      x++;
+      y++;
+    }
+    checker.clearBuffer();
+    reverseChecker.clearBuffer();
+  }
+
+  // Diagonal - Up right
+  for (let start = 0; start < allStarts; ++start) {
+    let x = Math.max(0, width - start - 1);
+    let y = Math.min(height - 1, allStarts - start - 1);
+    while (x < width && y >= 0) {
+      checker.read(input[y][x], x, y);
+      reverseChecker.read(input[y][x], x, y);
+      x++;
+      y--;
+    }
+    checker.clearBuffer();
+    reverseChecker.clearBuffer();
+  }
+
+  const middlePoints = checker.locationMatches.map((locations) => locations[1]);
+  const reverseMiddlePoints = reverseChecker.locationMatches.map(
+    (locations) => locations[1],
+  );
+
+  const results: Record<string, number> = {};
+  [...middlePoints, ...reverseMiddlePoints].forEach((point) => {
+    const key = `${point[0]},${point[1]}`;
+    if (results[key]) {
+      results[key] += 1;
+    } else {
+      results[key] = 1;
+    }
+  });
+
+  return Object.values(results).filter((result) => result === 2).length;
+};
+
 // Checker - Receives input stream, matches it against target
 class Checker {
   private target: string;
   private buffer: string = "";
+  private locationBuffer: [number, number][] = [];
   private index: number = 0;
   matches: number = 0;
+  locationMatches: [number, number][][] = [];
 
   constructor(target: string) {
     this.target = target;
   }
 
-  read(char: string) {
+  read(char: string, x: number, y: number) {
     if (this.target[this.index] === char) {
       this.buffer += char;
       this.index += 1;
+      this.locationBuffer.push([x, y]);
 
       if (this.buffer === this.target) {
         this.matches += 1;
+        this.locationMatches.push([...this.locationBuffer]);
         this.clearBuffer();
       }
     } else {
       if (char === this.target[0]) {
         this.buffer = char;
         this.index = 1;
+        this.locationBuffer = [[x, y]];
       } else {
         this.clearBuffer();
       }
@@ -101,12 +160,14 @@ class Checker {
   clearBuffer() {
     this.index = 0;
     this.buffer = "";
+    this.locationBuffer = [];
   }
 }
 
 const main = () => {
   const data = readFile(process.argv[2]);
-  const result = stepper(data, "XMAS");
+  // const result = stepper(data, "XMAS");
+  const result = xmasFinder(data, "MAS");
   console.log(result);
 };
 
