637bbaf938ddac6c38c5ebbfa1a6a6f9e3b74c6a
Niku Grönberg <gronbergniku@gmail.com>
2024-12-06 12:53:17 +0200

    kind of works

--- day-6/main.ts
+++ day-6/main.ts
@@ -39,12 +39,19 @@ const hash = (vector: [number, number]) => {
   return `${vector[0]},${vector[1]}`;
 };
 
+const hashWithDirection = (
+  vector: [number, number],
+  direction: [number, number],
+) => {
+  return `${vector[0]},${vector[1]} ${direction[0]},${direction[1]}`;
+};
+
 const readFile = (filename: string) => {
   const lines = fs.readFileSync(filename, "utf8").split("\n").slice(0, -1);
   return lines;
 };
 
-const guard = (map: string[]) => {
+const getStartPoint = (map: string[]): [number, number] => {
   let startPoint: [number, number] = [0, 0];
   for (let y = 0; y < map.length; ++y) {
     for (let x = 0; x < map[y].length; ++x) {
@@ -55,6 +62,10 @@ const guard = (map: string[]) => {
     }
   }
 
+  return startPoint;
+};
+
+const guard = (map: string[], startPoint: [number, number]) => {
   const visited = new Set<string>();
   visited.add(hash(startPoint));
 
@@ -82,13 +93,134 @@ const guard = (map: string[]) => {
     nextPoint = potentialNextPoint;
   }
 
-  return visited
+  return visited;
+};
+
+const logMap = (map: string[]) => {
+  map.forEach((row) => console.log(row));
+  console.log();
+};
+
+const guard2 = (map: string[], startPoint: [number, number]) => {
+  const visited = new Set<string>();
+  visited.add(hashWithDirection(startPoint, directions.up));
+  const pointsToCheck: { pos: [number, number]; dir: [number, number] }[] = [];
+  pointsToCheck.push({ pos: startPoint, dir: directions.up });
+
+  let direction = directions.up;
+
+  let nextPoint = add(startPoint, direction);
+  while (true) {
+    visited.add(hashWithDirection(nextPoint, direction));
+    pointsToCheck.push({ pos: nextPoint, dir: direction });
+
+    let potentialNextPoint = add(nextPoint, direction);
+    if (
+      potentialNextPoint[1] < 0 ||
+      potentialNextPoint[1] >= map.length ||
+      potentialNextPoint[0] < 0 ||
+      potentialNextPoint[0] >= map[0].length
+    ) {
+      break;
+    }
+
+    if (map[potentialNextPoint[1]][potentialNextPoint[0]] === "#") {
+      direction = nextDirection(direction);
+      potentialNextPoint = add(nextPoint, direction);
+    }
+
+    nextPoint = potentialNextPoint;
+  }
+
+  let results: Record<string, [number, number]> = {};
+  for (const point of pointsToCheck) {
+    const obstructionPosition = add(point.pos, point.dir);
+    if (equals(obstructionPosition, startPoint)) {
+      continue;
+    }
+
+    if (
+      obstructionPosition[1] < 0 ||
+      obstructionPosition[1] >= map.length ||
+      obstructionPosition[0] < 0 ||
+      obstructionPosition[0] >= map[0].length
+    ) {
+      continue;
+    }
+
+    if (map[obstructionPosition[1]][obstructionPosition[0]] === "#") {
+      continue;
+    }
+
+    const newMap = addObstruction(map, obstructionPosition);
+    const newDirection = nextDirection(point.dir);
+    const hasLoop = checkForLoop(newMap, point.pos, newDirection);
+    if (hasLoop) {
+      // logMap(newMap);
+      results[hash(obstructionPosition)] = obstructionPosition;
+    }
+  }
+
+  return Object.keys(results);
+};
+
+const addObstruction = (map: string[], position: [number, number]) => {
+  const copy = [...map];
+
+  let newRow = "";
+  const previousRow = copy[position[1]];
+  for (let i = 0; i < previousRow.length; ++i) {
+    newRow += i === position[0] ? "#" : previousRow[i];
+  }
+
+  copy[position[1]] = newRow;
+  return copy;
+};
+
+const checkForLoop = (
+  map: string[],
+  startPoint: [number, number],
+  startDirection: [number, number],
+) => {
+  const visited = new Set<string>();
+  visited.add(hashWithDirection(startPoint, startDirection));
+
+  let direction = startDirection;
+
+  let nextPoint = add(startPoint, direction);
+  while (true) {
+    const nextHash = hashWithDirection(nextPoint, direction);
+    if (visited.has(nextHash)) {
+      return true;
+    }
+
+    visited.add(nextHash);
+
+    let potentialNextPoint = add(nextPoint, direction);
+    if (
+      potentialNextPoint[1] < 0 ||
+      potentialNextPoint[1] >= map.length ||
+      potentialNextPoint[0] < 0 ||
+      potentialNextPoint[0] >= map[0].length
+    ) {
+      return false;
+    }
+
+    if (map[potentialNextPoint[1]][potentialNextPoint[0]] === "#") {
+      direction = nextDirection(direction);
+      potentialNextPoint = add(nextPoint, direction);
+    }
+
+    nextPoint = potentialNextPoint;
+  }
 };
 
 const main = () => {
   const map = readFile(process.argv[2]);
-  const visited = guard(map);
-  console.log(visited.size)
+  const startPoint = getStartPoint(map);
+  const loops = guard2(map, startPoint);
+  // console.log(loops);
+  console.log(loops.length);
 };
 
 main();
