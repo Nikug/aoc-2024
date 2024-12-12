94081d0b15401f12b93effd2f1f19430a26f45d2
Niku Grönberg <gronbergniku@gmail.com>
2024-12-12 21:50:53 +0200

    day 12 second solution

--- day-12/main.ts
+++ day-12/main.ts
@@ -17,6 +17,7 @@ interface Block {
 interface Field {
   letter: string;
   blocks: Record<string, Block>;
+  sides: number;
 }
 
 const readFile = (filename: string): string[] => {
@@ -51,6 +52,7 @@ const findField = (
   const field: Field = {
     letter: map[start[1]][start[0]],
     blocks: {},
+    sides: 0,
   };
 
   const positions: Vector[] = [start];
@@ -109,16 +111,106 @@ const fieldSpecs = (field: Field): { area: number; fences: number } => {
   };
 };
 
+const fieldBounds = (field: Field) => {
+  const blocks = Object.values(field.blocks);
+  let minX = blocks[0].position[0];
+  let minY = blocks[0].position[1];
+  let maxX = blocks[0].position[0];
+  let maxY = blocks[0].position[1];
+
+  blocks.forEach((block) => {
+    if (block.position[0] < minX) minX = block.position[0];
+    if (block.position[0] > maxX) maxX = block.position[0];
+    if (block.position[1] < minY) minY = block.position[1];
+    if (block.position[1] > maxY) maxY = block.position[1];
+  });
+
+  return { minX, minY, maxX, maxY };
+};
+
+const calculateFieldSides = (field: Field, map: string[]) => {
+  const { minX, minY, maxX, maxY } = fieldBounds(field);
+  const height = map.length;
+  const width = map[0].length;
+
+  const calculateSides = (
+    x: number,
+    y: number,
+    direction: Vector,
+    state: { isSide: boolean },
+  ) => {
+    let newIsSide = false;
+    const positionHash = hash([x, y]);
+    const neighbour = add([x, y], direction);
+
+    if (field.blocks[positionHash] && !isInBounds(width, height, neighbour)) {
+      newIsSide = true;
+    } else if (
+      field.blocks[positionHash] &&
+      map[neighbour[1]][neighbour[0]] !== field.letter
+    ) {
+      newIsSide = true;
+    } else {
+      newIsSide = false;
+    }
+
+    if (newIsSide && !state.isSide) {
+      field.sides += 1;
+    }
+
+    state.isSide = newIsSide;
+  };
+
+  // For every horizontal line in bounds
+  for (let y = minY; y <= maxY; ++y) {
+    // Check above and below blocks
+    for (const direction of [directions[0], directions[2]]) {
+      const state = { isSide: false };
+      // For every block in line
+      for (let x = minX; x <= maxX; ++x) {
+        calculateSides(x, y, direction, state);
+      }
+    }
+  }
+
+  // For every vertical line in bounds
+  for (let x = minX; x <= maxX; ++x) {
+    // Check left and right blocks
+    for (const direction of [directions[1], directions[3]]) {
+      const state = { isSide: false };
+      // For every block in line
+      for (let y = minY; y <= maxY; ++y) {
+        calculateSides(x, y, direction, state);
+      }
+    }
+  }
+
+  return field;
+};
+
 const main = () => {
   const map = readFile(process.argv[2]);
   const fields = findAllFields(map);
   let result = 0;
-  fields.forEach((field) => {
-    const { area, fences } = fieldSpecs(field);
-    // console.log(field.letter, ":", area, fences);
-    result += area * fences;
-  });
+  // fields.forEach((field) => {
+  //   const { area, fences } = fieldSpecs(field);
+  //   // console.log(field.letter, ":", area, fences);
+  //   result += area * fences;
+  // });
 
+  fields.forEach((field) => {
+    const newField = calculateFieldSides(field, map);
+    const { area } = fieldSpecs(newField);
+    // console.log(
+    //   newField.letter,
+    //   area,
+    //   "*",
+    //   newField.sides,
+    //   "=",
+    //   newField.sides * area,
+    // );
+    result += newField.sides * area;
+  });
   console.log(result);
 };
 
