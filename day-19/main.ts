10b510a3635dcd784ed14357389b5bfef8224e06
niku <gronbergniku@gmail.com>
2024-12-23 19:52:04 +0200

    technically works, never finishes

--- day-19/main.ts
+++ day-19/main.ts
@@ -32,16 +32,64 @@ const solveArrangement = (arrangement: string, start: number, currentSolution: s
   return null
 }
 
+interface Solution {
+  parts: string[];
+}
+
+const hashSolution = (solution: string[]): string => solution.join(",")
+
+const solveArrangementAllPossibilities = (arrangement: string, start: number, currentSolution: string[], solutions: Record<string, Record<string, string[]>>, towels: Set<string>): string[] | null => {
+  for (let i = arrangement.length; i > start; i--) {
+    const part = arrangement.slice(start, i);
+    if (solutions[part]) {
+      for (const solution of Object.values(solutions[part])) {
+        let shouldRecurse = false;
+        const newSolution = [...currentSolution, ...solution];
+        const newSolutionString = newSolution.join("");
+        const hash = hashSolution(newSolution);
+
+        if (newSolutionString.length > arrangement.length) {
+          return null;
+        }
+
+        if (!solutions[newSolutionString]) {
+          solutions[newSolutionString] = { [hash]: newSolution }
+          shouldRecurse = true
+        } else if (!solutions[newSolutionString][hash]) {
+          solutions[newSolutionString][hash] = newSolution
+          shouldRecurse = true
+        } else if (towels.has(part)) {
+          shouldRecurse = true
+        }
+
+        if (shouldRecurse) {
+          solveArrangementAllPossibilities(arrangement, i, newSolution, solutions, towels);
+        }
+      }
+
+    }
+  }
+
+  return null
+}
+
 const solveArrangements = (towels: string[], arrangements: string[]) => {
   const solutions: Record<string, string> = {}
   towels.forEach(towel => solutions[towel] = towel)
 
+  const solutions2: Record<string, Record<string, string[]>> = {}
+  towels.forEach(towel => solutions2[towel] = ({ [towel]: [towel] }))
+
   const results: string[] = []
 
   for (const arrangement of arrangements) {
-    const result = solveArrangement(arrangement, 0, "", solutions);
-    if (result) results.push(result)
+    // const result = solveArrangement(arrangement, 0, "", solutions);
+    console.log("solving", arrangement)
+    solveArrangementAllPossibilities(arrangement, 0, [], solutions2, new Set(towels))
   }
+
+  const foundSolutions = arrangements.map(arrangement => solutions2[arrangement]).filter(Boolean)
+  foundSolutions.forEach(solution => results.push(...Object.keys(solution)))
 
   return results
 }
