4f9234c97932405cc0b98fe2f4febfd7998ae429
Niku Grönberg <gronbergniku@gmail.com>
2025-02-01 11:52:39 +0200

    day 24 second solution

--- day-24/main.ts
+++ day-24/main.ts
@@ -7,8 +7,8 @@ interface Input {
 
 interface Gate {
   type: "AND" | "OR" | "XOR";
-  input1: string;
-  input2: string;
+  inputX: string;
+  inputY: string;
   output: string;
 }
 
@@ -42,8 +42,8 @@ const readFile = (filename: string) => {
       const parts = line.split(" ");
       const gate: Gate = {
         type: parts[1] as Gate["type"],
-        input1: parts[0],
-        input2: parts[2],
+        inputX: parts[0].startsWith("x") ? parts[0] : parts[2],
+        inputY: parts[2].startsWith("y") ? parts[2] : parts[0],
         output: parts[4],
       };
       gates.push(gate);
@@ -61,7 +61,7 @@ const simulate = (inputs: Input[], gates: Gate[]) => {
         output: input.id,
         value: input.value,
         configuration: input,
-      }),
+      })
   );
 
   const unsolvedGates = [...gates];
@@ -69,8 +69,8 @@ const simulate = (inputs: Input[], gates: Gate[]) => {
   while (unsolvedGates.length > 0) {
     for (let i = 0; i < unsolvedGates.length; ++i) {
       const gate = unsolvedGates[i];
-      const input1 = components[gate.input1];
-      const input2 = components[gate.input2];
+      const input1 = components[gate.inputX];
+      const input2 = components[gate.inputY];
       if (!input1 || !input2) continue;
 
       let newValue = false;
@@ -114,11 +114,134 @@ const getSolution = (components: Record<string, Component>) => {
   return parseInt(result, 2);
 };
 
+// x00 XOR y00  -> z00
+// x01 XOR y01  -> AAA
+// x00 AND y00 -> BBB
+// AAA XOR BBB -> z01
+
+// x01 AND y01 -> CCC
+// AAA AND BBB -> DDD
+// CCC OR DDD -> EEE
+
+// Allowed outputs are only XOR and 1 OR
+
+const fixGates = (inputs: Input[], gates: Gate[]) => {
+  const inputMap = inputs.reduce<Record<string, Input>>((acc, input) => ({ ...acc, [input.id]: input }), {});
+
+  let carry: Gate | null = null;
+  for (let i = 0; i < 44; ++i) {
+    console.log("Checking bit", i);
+    const paddedNumber = i.toString().padStart(2, "0");
+    const inputX = inputMap[`x${paddedNumber}`];
+    const inputY = inputMap[`y${paddedNumber}`];
+
+    // Check half adder
+    if (carry === null) {
+      const outputGate = gates.find(
+        (gate) =>
+          gate.type === "XOR" &&
+          gate.inputX === inputX.id &&
+          gate.inputY === inputY.id &&
+          gate.output === `z${paddedNumber}`
+      );
+
+      const carryGate = gates.find(
+        (gate) => gate.inputX === inputX.id && gate.inputY === inputY.id && gate.type === "AND"
+      );
+      if (outputGate && carryGate) {
+        carry = carryGate;
+      } else {
+        console.log("Something wrong", carryGate, outputGate);
+        return;
+      }
+    }
+    // Check full adder
+    else {
+      const inputXor = gates.find(
+        (gate) => gate.inputX === inputX.id && gate.inputY === inputY.id && gate.type === "XOR"
+      );
+      const inputAnd = gates.find(
+        (gate) => gate.inputX === inputX.id && gate.inputY === inputY.id && gate.type === "AND"
+      );
+
+      if (!inputXor) {
+        console.log("missing input xor");
+        return;
+      }
+
+      if (!inputAnd) {
+        console.log("missing input and");
+        return;
+      }
+
+      const inputXorAndCarryXor = gates.find(
+        (gate) =>
+          (gate.inputX === inputXor!.output || gate.inputX === carry!.output) &&
+          (gate.inputY === inputXor!.output || gate.inputY === carry!.output) &&
+          gate.type === "XOR"
+      );
+      const inputXorAndCarryAnd = gates.find(
+        (gate) =>
+          (gate.inputX === inputXor!.output || gate.inputX === carry!.output) &&
+          (gate.inputY === inputXor!.output || gate.inputY === carry!.output) &&
+          gate.type === "AND"
+      );
+
+      if (!inputXorAndCarryXor) {
+        console.log("missing input xor and carry xor");
+        console.log("inputXor", inputXor);
+        console.log("carry", carry);
+        return;
+      }
+
+      if (!inputXorAndCarryAnd) {
+        console.log("missing input xor and carry and");
+        return;
+      }
+
+      const nextCarry = gates.find(
+        (gate) =>
+          (gate.inputX === inputXorAndCarryAnd!.output || gate.inputX === inputAnd!.output) &&
+          (gate.inputY === inputXorAndCarryAnd!.output || gate.inputY === inputAnd!.output) &&
+          gate.type === "OR"
+      );
+
+      if (!nextCarry) {
+        console.log("missing next carry");
+        console.log("input xor and carry and", inputXorAndCarryAnd);
+        console.log("input xor and carry xor", inputXorAndCarryXor);
+        console.log("input and", inputAnd);
+        console.log("previous carry", carry);
+        return;
+      }
+
+      if (nextCarry.output.startsWith("z") && i !== 43) {
+        console.log("incorrect next carry", nextCarry);
+        console.log("input xor and carry and", inputXorAndCarryAnd);
+        console.log("input xor and carry xor", inputXorAndCarryXor);
+        console.log("input and", inputAnd);
+        return;
+      }
+
+      carry = nextCarry;
+    }
+  }
+};
+
+// Swaps:
+// 1. z08, mvb
+// 2. rds, jss
+// 3. z18, wss
+// 4. z23, bmn
+
 const main = () => {
   const { inputs, gates } = readFile(process.argv[2]);
-  const simulationResult = simulate(inputs, gates);
-  const result = getSolution(simulationResult);
-  console.log(result);
+  // const simulationResult = simulate(inputs, gates);
+  // const result = getSolution(simulationResult);
+  // console.log(result);
+
+  fixGates(inputs, gates);
+  console.log(["z08", "mvb", "rds", "jss", "z18", "wss", "z23", "bmn"].sort().join(","));
 };
 
 main();
